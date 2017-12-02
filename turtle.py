994fd35ea9b7d6bd1d1de74cc4eb24f2bcbcd376
kosmo5-12 <kwlsy666@naver.com>
2017-12-02 16:17:55 +0900

    sample

---
+++ turtle.py
@@ -0,0 +1,44 @@
+# import turtle
+# t = turtle.Turtle()
+#
+# t.shape("turtle")
+#
+# radius = 100
+# t.circle(radius)
+# t.fd(30)
+# t.circle(radius)
+# t.fd(30)
+# t.circle(radius)
+# name = input("이름을 입력하시오: ")
+#
+# print(name, "씨, 안녕하세요?")
+#
+# print("파이썬에 오신 것을 환영합니다.")
+#
+# a = int(input("첫 번째 정수를 입력하시오: "))
+#
+# b = int(input("두 번째 정수를 입력하시오: "))
+# sum = a + b
+#
+# print("%d와 %d의 합은 %d입니다."%(a,b,sum))
+
+print("Hello, Python")
+print('Hello, Python')
+
+food = 'Python\'s favorite food is perl'
+say = "\"Python is very easy.\" he says."
+
+print(food)
+print(say)
+
+#
+multiline='''
+...Life is too short
+...You need python
+...'''
+print(multiline)
+
+a = "python"
+print(a*2)
+
+
